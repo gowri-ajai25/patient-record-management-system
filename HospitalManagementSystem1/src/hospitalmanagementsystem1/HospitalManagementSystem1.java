140dd6ebf5cb44ddb267076bba0a99dc4c87d18e
gowri-ajai25 <gowriajai2023@gmail.com>
2026-05-02 13:04:04 +0530

    Initial commit - Patient Record Management System

---
+++ HospitalManagementSystem1/src/hospitalmanagementsystem1/HospitalManagementSystem1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hospitalmanagementsystem1;
+
+/**
+ *
+ * @author Chanj
+ */
+public class HospitalManagementSystem1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
